b24d7282a959acba69d5c44026dcf77e09227c4c
usmanaliyu <osman.abbati@gmail>
2020-05-30 19:58:19 +0200

    first host

--- core/admin.py
+++ core/admin.py
@@ -12,6 +12,7 @@ make_refund_accepted.short_description = 'Update orders to refund granted'
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['user',
+                    'order_id',
                     'ordered',
                     'being_delivered',
                     'received',
@@ -55,13 +56,32 @@ class AddressAdmin(admin.ModelAdmin):
     list_filter = ['default', 'address_type', 'country']
     search_fields = ['user', 'street_address', 'apartment_address', 'zip']
 
+class ItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
 
-admin.site.register(Item)
-admin.site.register(OrderItem)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'order_id',
+        'quantity',
+        'item',
+
+        
+    ]
+
+admin.site.register(Item,ItemAdmin)
+admin.site.register(OrderItem,OrderItemAdmin)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Payment)
 admin.site.register(Coupon)
 admin.site.register(Refund)
 admin.site.register(Address, AddressAdmin)
 admin.site.register(UserProfile)
-admin.site.register(Category)
+admin.site.register(Category,CategoryAdmin)
+
+admin.site.site_header = "Jane's Admin"
+admin.site.site_text = "Jane's Admin Portal"
+admin.site.site_title = "Welcome to janes Admin Portal"
--- core/models.py
+++ core/models.py
@@ -81,6 +81,7 @@ class Item(models.Model):
 class OrderItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
+    order_id = models.AutoField(primary_key=True)
     ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
@@ -106,6 +107,7 @@ class OrderItem(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
+    order_id = models.AutoField(primary_key=True)
     ref_code = models.CharField(max_length=20, blank=True, null=True)
     items = models.ManyToManyField(OrderItem)
     start_date = models.DateTimeField(auto_now_add=True)
